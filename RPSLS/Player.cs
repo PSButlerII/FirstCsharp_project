113a4ab053d8740a6cbf5743b810afed94cb149c
PSButler <38290715+PSButlerII@users.noreply.github.com>
2021-08-05 15:58:28 -0400

    created a player class and added four variables

---
+++ RPSLS/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPSLS
+{
+    class Player
+    {
+        //member variable
+        public string name;
+        public string rock;
+        public string paper;
+        public string scissors;
+        public string lizard;
+        public string spoke;
+        public int wins;
+        public int lose;
+        public int tie;
+        public bool win;
+
+
+
+
+        //constructor
+
+
+
+
+
+        //member methods
+    }
+}
