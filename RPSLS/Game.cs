093e47b94224836e134b78079c0698fa1264548a
Preston Butler <prestonsbutler@gmail.com>
2023-11-29 13:58:53 -0500

    Added a game class, and updated the player class.  close to finishing this C# project.

---
+++ RPSLS/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPSLS
+{
+    class Game
+    {
+        Player player1;
+        Player player2;
+
+        public Game()
+        {
+            // Initialize players
+            player1 = new Player("Player 1", true);  // Human player
+            player2 = new Player("CPU", false);     // CPU player
+        }
+
+        public void StartGame()
+        {
+            // Main game logic, like playing rounds, checking scores, etc.
+        }
+
+       
+    }
+}
--- RPSLS/Player.cs
+++ RPSLS/Player.cs
@@ -6,39 +6,39 @@ namespace RPSLS
 {
     class Player
     {
-        //member variable
-        public string name;        
+        public string name;
         public int score;
-        public bool isHuman;        
+        public bool isHuman;
         public string chosenGesture;
-        public List<string> gestures = new List<string>() { "rock", "paper", "scissors", "lizard", "spoke" };
+        public List<string> gestures = new List<string>() { "rock", "paper", "scissors", "lizard", "spock" };
 
-
-        //constructor
-      
-
-        
-
-
-        //member methods
-
-        public void Human()
+        public Player(string name, bool isHuman)
         {
-            isHuman = true;
+            this.name = name;
+            this.isHuman = isHuman;
+            this.score = 0;
         }
-        public void CPU()
-        {
-            isCPU = true;
-        }
+
         public void pickGesture()
-
         {
-            Console.WriteLine("pick a gesture:");
-            chosenGesture = Console.ReadLine();
+            if (isHuman)
+            {
+                Console.WriteLine($"{name}, pick a gesture (rock, paper, scissors, lizard, spock):");
+                chosenGesture = Console.ReadLine().ToLower();
+            }
+            else
+            {
+                RandomNumber();
+            }
         }
+
         public void RandomNumber()
         {
-
+            Random rnd = new Random();
+            int index = rnd.Next(gestures.Count);
+            chosenGesture = gestures[index];
+            Console.WriteLine($"{name} (CPU) picked: {chosenGesture}");
         }
     }
+
 }
